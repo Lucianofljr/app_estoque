58a4b1e614a58332228a89068c7bc7a779717e9d
Lucianofljr <lucianoflirajr@gmail.com>
2024-10-04 11:47:52 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,97 @@
+from flet import *
+import sqlite3 as sl 
+
+
+connect = sl.connect("dados.db", check_same_thread=False)
+cursor = connect.cursor()
+
+
+def table_warehouse():
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS produtos (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        lote TEXT,
+        produto TEXT,
+        validade TEXT,
+        quantidade INTEGER)
+'''
+    )
+
+
+class App(UserControl):
+    def __init__(self):
+        super().__init__()
+
+        # def validation(e):
+        #     self.update()
+        #     return e
+
+        self.warehouse = Column(auto_scroll=True)
+        self.create_lote = TextField(label="#lote: ")
+        self.value_lote = TextField(label="Quantidade produzida: ")
+        self.update_prod = TextField(label="Editar quantidade: ")
+        # self.shelf_life = DatePicker(open=True)
+        self.list_prod = Dropdown(options=[
+            dropdown.Option("Ghee tradicional 200g"),
+            dropdown.Option("Ghee tradicional 300g"),
+            dropdown.Option("Ghee tradicional 500g"),
+            dropdown.Option("Ghee tradicional refil 300g"),
+            dropdown.Option("Ghee sal do himalaia 200g"),
+            dropdown.Option("Ghee sal do himalaia 300g"),
+            dropdown.Option("Ghee sal do himalaia 500g"),
+            dropdown.Option("Ghee sal do himalaia refil 300g"),
+            dropdown.Option("Ghee douradíssimo 200g"),
+            dropdown.Option("Ghee douradíssimo 300g"),
+            dropdown.Option("Ghee douradíssimo 500g"),
+            dropdown.Option("Ghee alho 200g"),
+            dropdown.Option("Ghee cúrcuma 200g"),
+            dropdown.Option("Caldo de legumes 350ml"),
+            dropdown.Option("Caldo de legumes 600ml"),
+            dropdown.Option("GheeRofa 250g"),          
+            ])
+
+
+    def build(self):
+        button_create_lote = ElevatedButton('Inserir Estoque', on_click=self.create_new_lote)
+
+        post_lote = Container(Row(
+                controls=[self.create_lote], alignment=MainAxisAlignment.CENTER,
+        ))
+
+        # post_shelf_life = Container(Row(
+        #         controls=[Text("Data: "),self.shelf_life], alignment=MainAxisAlignment.CENTER,
+        # ))
+    
+        post_prod = Container(Row(
+                controls=[self.list_prod], alignment=MainAxisAlignment.CENTER,
+        ))
+    
+        post_quant = Container(Row(
+                controls=[self.value_lote], alignment=MainAxisAlignment.CENTER,
+        ))
+
+        return Column([
+            Text("Atualização do Estoque", size=20, weight="bold"),
+            post_lote, post_prod, post_quant, button_create_lote,
+        ], 
+        alignment=MainAxisAlignment.SPACE_AROUND,
+        horizontal_alignment=CrossAxisAlignment.CENTER,
+        )
+    
+
+    def create_new_lote(self, e):
+        cursor.execute("INSERT TO produtos ()")
+        self.warehouse.controls.append(Text(f"Novo lote ({self.create_lote.value}): {self.value_lote.value} unidades."))
+        self.create_lote.value = ""
+        self.update()
+
+
+def main(page:Page):
+
+    my_app = App()
+    
+    
+    page.controls.append(my_app)
+    page.update()
+
+app(target=main)
